522fa0a1b7b98fe08f6f430acf6856cdcdc5b08e
DanielBerg932 <76453871+DanielBerg932@users.noreply.github.com>
2021-01-31 17:00:51 +0100

    Add files via upload

---
+++ clean clock/javascript/script.js
@@ -0,0 +1,35 @@
+let hoursid = document.getElementById('hours');
+let minutesid = document.getElementById('minutes');
+let secondsid = document.getElementById('seconds');
+let miliid=document.getElementById('mili')
+
+
+setInterval(function()
+{
+    var today= new Date;
+    var hours=today.getHours(); 
+    var minutes=today.getMinutes();
+    var seconds= today.getSeconds();
+    var mili=today.getMilliseconds();
+    if (hours<10)
+        {
+            hours='0'+today.getHours();
+        } 
+        if (minutes<10)
+        {
+            minutes='0'+today.getMinutes();
+        } 
+        if (seconds<10)
+        {
+            seconds='0'+today.getSeconds();
+        }  
+        if (mili<10)
+        {
+            mili='0'+today.getSeconds();
+        } 
+    hoursid.textContent=hours;
+    minutesid.textContent=minutes
+    secondsid.textContent=seconds;
+   // miliid.textContent=mili;remove for miliseconds
+
+},1000);
